5e93661f8e63271f718c753bed8f000df2b29be8
hiihii7319-spec <hiihii7319@gmail.com>
2025-11-04 11:07:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2876,6 +2876,14 @@ sub_conv = ConversationHandler(
         fallbacks=global_fallbacks + user_menu_fallback,
         allow_reentry=True
     )
+sub_conv = ConversationHandler(
+        entry_points=[CallbackQueryHandler(user_upload_ss_start, pattern="^user_upload_ss$")],
+        states={
+            SUB_GET_SCREENSHOT: [MessageHandler(filters.PHOTO, user_get_screenshot)],
+        },
+        fallbacks=global_fallbacks + user_menu_fallback,
+        allow_reentry=True
+    )
     
     admin_approve_conv = ConversationHandler(
         entry_points=[CallbackQueryHandler(admin_approve_start, pattern="^admin_approve_")],
