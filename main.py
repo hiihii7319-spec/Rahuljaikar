185f105c2c01a2b70c886db799987069dc4319cd
hiihii7319-spec <hiihii7319@gmail.com>
2025-11-05 14:50:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3238,7 +3238,7 @@ async def download_button_handler(update: Update, context: ContextTypes.DEFAULT_
             else:
                     await context.bot.send_message(user_id, msg)
         except Exception: pass
-
+            
 # --- Error Handler ---
 async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.error(f"Error: {context.error} \nUpdate: {update}", exc_info=True)
